7c3b5d21a52528764bc67a1424390822cf4489a8
Purushottam Bingi <deepakbingi16@gmail.com>
2023-02-10 01:00:06 +0530

    Done

--- src/components/AddClients.js
+++ src/components/AddClients.js
@@ -35,7 +35,7 @@ const AddClients = ({data,setData}) => {
       }
     return (
         <div>
-            <span id='addClients' onClick={handleClick}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M5.22 8.22a.749.749 0 0 0 0 1.06l6.25 6.25a.749.749 0 0 0 1.06 0l6.25-6.25a.749.749 0 1 0-1.06-1.06L12 13.939 6.28 8.22a.749.749 0 0 0-1.06 0Z"></path></svg><BsFillPlusSquareFill></BsFillPlusSquareFill></span>
+            <span id='addClients' onClick={handleClick}><BsFillPlusSquareFill></BsFillPlusSquareFill></span>
             {clicked ?<form className='addInput' onSubmit={handleForm}>
                 <input type="text" placeholder='Company Name' className="inputs" value={name} onChange={(e)=>{setName(e.target.value)}}></input>
                 <input type="text" placeholder='Domain' className="inputs" value={domain} onChange={(e)=>{setDomain(e.target.value)}}></input>
